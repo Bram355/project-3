09ac6a715e5d9828f087bad55c1fb1c6d23ffd83
Bramwel <bramwel.mutngati@student.moringaschool.com>
2025-05-28 19:17:43 +0300

    added key on project.py

--- freelance_manager/models/project.py
+++ freelance_manager/models/project.py
@@ -2,14 +2,19 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from freelance_manager.database import Base
 
+
+
 class Project(Base):
     __tablename__ = 'projects'
+
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
-    start_date = Column(Date)
-    end_date = Column(Date)
-    client_id = Column(Integer, ForeignKey('clients.id'))
+
+    client_id = Column(Integer, ForeignKey('clients.id')) 
+
+    client = relationship("Client", back_populates="projects")
+
 
     client = relationship("Client", back_populates="projects")
     invoices = relationship("Invoice", back_populates="project")
